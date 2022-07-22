4e73b0dcdb38ca55312fb87d684250613bdb7ae3
Marcelo Rodrigues <marcelorodrigues@MacBook-Air-de-Marcelo.local>
2022-07-22 20:53:41 -0300

    atualizacoes de versionamento de codigo

---
+++ README.md
@@ -0,0 +1,28 @@
+## PARA COMEÇAR o PROJETO APITEST
+instale as dependencias :
+npm install
+
+ou
+yarn install
+
+## TECNOLOGIAS UTILIZADAS
+Next js
+React js
+Tailwind Css
+Dados considos de uma Api do Firebase com base de dados criada para o teste
+
+## FUNCIONALIDADES
+tela inicial com dados exibidos da api do banco de dados opção para criar novo cadastro (botão)
+tela de novo cadastro opção para alterar dados com (icone)
+tela de alterar dados opção de excluir cadastro
+responsividade usando o tailwind
+Criar arquivo ".env" se ele não estiver no repositório, para variáveis de ambiente.
+adicionar o conteúdo abaixo, salvar e iniciar o projeto.
+
+NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyA42HzzQjL596CjCLx058vnAnkBw7c4CxA NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=cliente-50044.firebaseapp.com NEXT_PUBLIC_FIREBASE_PROJECT_ID=cliente-50044
+
+## PARA EXECUTAR O PROJETO
+npm run dev
+
+or
+yarn run dev
---
+++ src/pages/index.tsx
@@ -0,0 +1,51 @@
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+
+import useClientes from "../hooks/useClientes";
+
+export default function Home() {
+
+  const {
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecionarCliente,
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
+
+  return (
+    <div className={`
+      flex flex-1 justify-center items-center w-full h-screen
+      bg-gradient-to-r from-blue-700 to-blue-400
+      text-white
+    `}>
+      <Layout titulo="Dados da Api ">
+        {tabelaVisivel ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor="blue" className="mb-4"
+                onClick={novoCliente}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
+            />
+          </>
+        ) : (
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={exibirTabela}
+          />
+        )}
+      </Layout>
+    </div>
+  )
+}
